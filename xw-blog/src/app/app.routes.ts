a4cf4a2a07f6a58567bc9acd1114c3a311ab48d6
ITSERVICES\jz043x <jz043x@att.com>
2026-04-01 10:22:22 -0500

    initial site structure

---
+++ xw-blog/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () => import('./pages/home/home-page.component').then((m) => m.HomePageComponent)
+	},
+	{
+		path: 'puzzle/:id',
+		loadComponent: () => import('./pages/puzzle/puzzle-page.component').then((m) => m.PuzzlePageComponent)
+	},
+	{
+		path: '**',
+		redirectTo: ''
+	}
+];
---
+++ xw-blog/src/app/pages/home/home-page.component.ts
@@ -0,0 +1,47 @@
+import { DatePipe } from '@angular/common';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { Crossword } from '../../models/crossword.model';
+import { CrosswordSheetService } from '../../services/crossword-sheet.service';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [RouterLink, DatePipe],
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+  private readonly crosswordSheetService = inject(CrosswordSheetService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected readonly crosswords = signal<Crossword[]>([]);
+  protected readonly isLoading = signal(true);
+  protected readonly errorMessage = signal('');
+
+  constructor() {
+    this.loadCrosswords();
+  }
+
+  protected loadCrosswords(): void {
+    this.isLoading.set(true);
+    this.errorMessage.set('');
+
+    this.crosswordSheetService
+      .getCrosswords()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (crosswords) => {
+          this.crosswords.set(crosswords);
+          this.isLoading.set(false);
+        },
+        error: () => {
+          this.errorMessage.set(
+            'Unable to load crossword posts from Google Sheets. Add your public sheet ID in the crossword service and try again.'
+          );
+          this.crosswords.set([]);
+          this.isLoading.set(false);
+        }
+      });
+  }
+}
